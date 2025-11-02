f8de58c23428e51644454dba1fd6c1bfae7248b9
Vrinda <yvrinda67@gmail.com>
2025-11-02 23:29:49 +0530

    Q13.c

---
+++ Q13.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (n == 1) {
+        printf("Peak element is %d (only one element in array)\n", arr[0]);
+        return 0;
+    }
+  
+    if (arr[0] >= arr[1]) {
+        printf("Peak element is %d (at index 0)\n", arr[0]);
+        return 0;
+    }
+
+    for (i = 1; i < n - 1; i++) {
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
+            printf("Peak element is %d (at index %d)\n", arr[i], i);
+            return 0;
+        }
+    }
+
+    if (arr[n - 1] >= arr[n - 2]) {
+        printf("Peak element is %d (at index %d)\n", arr[n - 1], n - 1);
+    } else {
+        printf("No peak element found.\n");
+    }
+
+    return 0;
+}
