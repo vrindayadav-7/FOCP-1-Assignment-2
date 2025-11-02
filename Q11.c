3d32f78c15b5dfb57ce2f2caf8cdfcd40edce8a0
Vrinda <yvrinda67@gmail.com>
2025-11-02 23:21:26 +0530

    Q11.c

---
+++ Q11.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int scores[100], even_array[100], odd_array[100];
+    int n, i, even_count = 0, odd_count = 0;
+
+    printf("Enter the number of scores: ");
+    scanf("%d", &n);
+
+
+    printf("Enter %d scores:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &scores[i]);
+    }
+
+    
+    for (i = 0; i < n; i++) {
+        if (scores[i] % 2 == 0) {
+            even_array[even_count] = scores[i];
+            even_count++;
+        } else {
+            odd_array[odd_count] = scores[i];
+            odd_count++;
+        }
+    }
+
+    printf("\nEven scores: ");
+    for (i = 0; i < even_count; i++) {
+        printf("%d ", even_array[i]);
+    }
+  
+    printf("\nOdd scores: ");
+    for (i = 0; i < odd_count; i++) {
+        printf("%d ", odd_array[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
